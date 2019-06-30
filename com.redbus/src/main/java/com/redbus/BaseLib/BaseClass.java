9e186b41f7e4fdde532ad7d84ca1236e9c5106ea
laxsp <laxsp@DESKTOP-7RJPU8Q>
2019-06-30 08:09:32 -0700

    Properties file implentation 30_06_2019

--- com.redbus/src/main/java/com/redbus/BaseLib/BaseClass.java
+++ com.redbus/src/main/java/com/redbus/BaseLib/BaseClass.java
@@ -1,5 +1,6 @@
 package com.redbus.BaseLib;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -8,44 +9,42 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
-public class BaseClass implements GlobalVariable{
-	
-WebDriver driver;
-@Parameters ({ "browser_Name" })
-@BeforeClass
+import com.redbus.Utills.ActionUtils;
 
-public void launchBrowser(String browser)
-{
-	if(browser.equalsIgnoreCase("chrome"))
-	{
-		System.setProperty(CHROME_KEY,CHROME_VALUE);
-		driver.get(URL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
-	}
-	else if (browser.equalsIgnoreCase("firefox")) {
-		System.setProperty(FIREFOX_KEY,FIREFOX_VALUE);
-		driver.get(URL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
-	}
-	else if (browser.equalsIgnoreCase("ie")) {
-		System.setProperty(IE_KEY,IE_VALUE);
-		driver.get(URL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
-	}
-	else
-	{
-		System.out.println("Kindly enter the browser name");
-	}
-}
+public class BaseClass implements GlobalVariable {
 
-@AfterClass
-public void tearDown()
-{
-	driver.manage().deleteAllCookies();
-	driver.quit();
-}
+	WebDriver driver;
+
+	@Parameters({ "browser_Name" })
+	@BeforeClass
+
+	public void launchBrowser(String browser) throws IOException {
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty(ActionUtils.getProertyValue(DRIVERSETUPFILEPATH, "chromeKey"),
+					ActionUtils.getProertyValue(DRIVERSETUPFILEPATH, "chromeValue"));
+			driver.get(ActionUtils.getProertyValue(CONFIGPRPERTIESFILEPATH, "url"));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			System.setProperty(ActionUtils.getProertyValue(DRIVERSETUPFILEPATH, "firefoxKey"),
+					ActionUtils.getProertyValue(DRIVERSETUPFILEPATH, "firefoxValue"));
+			driver.get(ActionUtils.getProertyValue(CONFIGPRPERTIESFILEPATH, "url"));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
+		} /*else if (browser.equalsIgnoreCase("ie")) {
+			System.setProperty(IE_KEY, IE_VALUE);
+			driver.get(URL);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
+		} */else {
+			System.out.println("Kindly enter a valid browser name");
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+		driver.quit();
+	}
 
 }
